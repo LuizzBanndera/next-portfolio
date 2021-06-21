95d09ec8b0faa4fdc25572ac716a8f7d03784d35
luizzbanndera <luizzbanndera@gmail.com>
2021-06-21 15:20:03 -0300

    added logo animation

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,10 +1,19 @@
 import Styles from './Header.module.scss'
 import Button from '@material-ui/core/Button'
+import { motion } from 'framer-motion'
 
 export default function Header() {
   return (
     <header className={Styles.Container}>
-      <button className={Styles.Logo}>LB</button>
+      <motion.div
+        whileHover={{
+          scale: 1.2,
+          transition: { duration: 0.3 },
+        }}
+        whileTap={{ scale: 0.9 }}
+      >
+        <button className={Styles.Logo}>LB</button>
+      </motion.div>
       <ul className={Styles.Menu}>
         <Button size="small">Home</Button>
         <Button size="small">Contact</Button>
